01e8e605f0766b6f7120e274b8e2032d7aba078d
JGodwyn <60453471+JGodwyn@users.noreply.github.com>
2024-10-01 10:01:42 +0100

    Initial Commit

---
+++ NewTestProject/NewTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewTestProjectApp.swift
+//  NewTestProject
+//
+//  Created by Gdwn on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
